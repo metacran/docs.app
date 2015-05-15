f6f70f0a01a3ca4306e8aa9a1c295e79b3a6d8ea
Gabor Csardi <csardi.gabor@gmail.com>
2015-05-15 09:06:52 -0400

    Simple API to add a package to the README queue

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-re_full = new RegExp('^/build/readme/([\\w\\.]+)$');
+var broker_url = process.env.BROKER_URL || 'amqp://localhost';
 
+var re_full = new RegExp('^/build/readme/([\\w\\.]+)$');
 router.get(re_full, function(req, res) {
     var package = req.params[0];
+    var q = 'readme';
+    var open = require('amqplib').connect(broker_url);
 
-    var open = require('amqplib').connect('amqp://localhost');
+    open.then(function(conn) {
+	var ok = conn.createChannel();
+	ok = ok.then(function(ch) {
+	    ch.assertQueue(q);
 
-    
-    res.send('OK')
+	    var entry = { 'package': package,
+			  'added_at': new Date().toISOString(),
+			  'added_by': 'docs.app' };
+
+	    ch.sendToQueue(q, new Buffer(JSON.stringify(entry)));
+	});
+	return ok;
+    }).then(null, console.warn);
+
+    res.set('Content-Type', 'application/json')
+	.send({ 'operation': 'add',
+		'type': 'readme',
+		'package': package,
+		'result': 'OK' })
 	.end();
 })
 
