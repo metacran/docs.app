2ac6daebebb1a07af8ca487276d04140fe17b62c
Gabor Csardi <csardi.gabor@gmail.com>
2015-05-15 20:56:57 -0400

    Close the connection properly
    Otherwise the server runs out of memory.

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var amqp = require('amqplib');
+var when = require('when');
 
 var broker_url = process.env.RABBITMQ_URL || 'amqp://localhost';
 
@@ -7,20 +9,21 @@ var re_full = new RegExp('^/build/readme/([\\w\\.]+)$');
 router.get(re_full, function(req, res) {
     var package = req.params[0];
     var q = 'readme';
-    var open = require('amqplib').connect(broker_url);
 
-    open.then(function(conn) {
-	var ok = conn.createChannel();
-	ok = ok.then(function(ch) {
-	    ch.assertQueue(q);
+    amqp.connect(broker_url).then(function(conn) {
+	return when(conn.createChannel().then(function(ch) {
+	    var ok = ch.assertQueue(q, { durable: true });
 
 	    var entry = { 'package': package,
 			  'added_at': new Date().toISOString(),
 			  'added_by': 'docs.app' };
 
-	    ch.sendToQueue(q, new Buffer(JSON.stringify(entry)));
-	});
-	return ok;
+	    return ok.then(function() {
+		var msg = JSON.stringify(entry);
+		ch.sendToQueue(q, new Buffer(msg), { deliveryMode: true });
+		return ch.close();
+	    });
+	})).ensure(function() { conn.close(); });
     }).then(null, console.warn);
 
     res.set('Content-Type', 'application/json')
