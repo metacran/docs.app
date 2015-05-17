877d45ecb01fa64a2b622160419f9875d0fe6f38
Gabor Csardi <csardi.gabor@gmail.com>
2015-05-16 21:29:12 -0400

    Rebuild all existing READMEs

--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 var amqp = require('amqplib');
 var when = require('when');
+var request = require('request');
 
 var broker_url = process.env.RABBITMQ_URL || 'amqp://localhost';
+var q = 'readme';
 
 var re_full = new RegExp('^/build/readme/([\\w\\.]+)$');
 router.get(re_full, function(req, res) {
     var package = req.params[0];
-    var q = 'readme';
 
     amqp.connect(broker_url).then(function(conn) {
 	return when(conn.createChannel().then(function(ch) {
@@ -32,6 +33,54 @@ router.get(re_full, function(req, res) {
 		'package': package,
 		'result': 'OK' })
 	.end();
+})
+
+// Rebuild all existing READMEs
+
+router.get('/build/readme/rebuild-existing', function(req, res) {
+    var couch_url = process.env.DOCSDB_URL || 'http://127.0.0.1:5984';
+    var url = couch_url + '/readme/_all_docs';
+    request(url, function(error, response, body) {
+	if (error || response.statusCode != 200) {
+	    res.set('Content-Type', 'application/json')
+		.set(500)
+		.send({ "result": "error",
+			"error": error })
+		.end();
+	}
+	var docs = JSON.parse(body)
+	    .rows
+	    .map(function(x) { return x["id"]; });
+
+	amqp.connect(broker_url).then(function(conn) {
+	    return when(conn.createChannel().then(function(ch) {
+		var ok = ch.assertQueue(q, { durable: true })
+		    .then(function() { return 0; });
+
+		for (p in docs) {
+		    ok = ok.then(function(p) {
+			var package = docs[p];
+			var entry = { 'package': package,
+				      'added_at': new Date().toISOString(),
+				      'added_by': 'docs.app/rebuild-existing' };
+			var msg = JSON.stringify(entry);
+			console.log(entry);
+			ch.sendToQueue(q, new Buffer(msg),
+				       { deliveryMose: true });
+			return p + 1;
+		    });
+		}
+
+		return ok.then(function() { return ch.close(); });
+	    })).ensure(function() { conn.close(); });
+	}).then(null, console.warn);
+
+	res.set('Content-Type', 'application/json')
+	    .send({ 'operation': 'rebuild-existing',
+		    'type': 'readme',
+		    'result': 'OK' })
+	    .end();
+    })
 })
 
 module.exports = router;
